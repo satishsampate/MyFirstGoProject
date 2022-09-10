55383473add845deefc7e781be5c0a316ac958f0
satishsampate <97726560+satishsampate@users.noreply.github.com>
2022-09-10 12:03:08 +0530

    First commit in add_token_files

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"com/tutorials/helpers"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("First go program")
+
+	var myStudnet helpers.Student
+
+	myStudnet.Name = " name sirname"
+	myStudnet.RollNumber = 45
+	fmt.Printf("Stuend name is %s and his roll number is %d", myStudnet.Name, myStudnet.RollNumber)
 }
